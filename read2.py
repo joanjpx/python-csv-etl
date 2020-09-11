75e725181a07219b6ffa07cc375b6b013f5b6edc
Joan Perez Alvarado <joansyn.s@gmail.com>
2020-09-11 13:58:28 -0500

    Update read2.py

--- read2.py
+++ read2.py
@@ -4,9 +4,15 @@ from Database import Database
 with open('elements.csv') as File:  
     reader = csv.reader(File)
     for row in reader:
-        print(row[1])
+        #print(row[1])
         m = Database()
-        m.selectOne(row[1])
+        rID = m.selectOne(row[1])
+        if rID!=False:
+            m.updateRow(rID,row[3])
+        else:
+            print("No Match")
+            
+            
     	
     	
 
